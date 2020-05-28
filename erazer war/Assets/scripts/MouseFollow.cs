9a0cb7b57f572a68612fc74863a9f15f4837679a
Salman Arshad <k163898@nu.du.pk>
2020-05-28 20:04:25 +0500

    issue with PointB

---
+++ erazer war/Assets/scripts/MouseFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    private float offset = -0.8f;
+    private Vector3 temPos;
+    void Start()
+    {
+        transform.position  =  new Vector3(transform.position.x,transform.position.y,offset);
+    }
+
+    void Update()
+    {
+        temPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10));
+        transform.position = new Vector3(temPos.x,2.899f,temPos.z);
+    }
+}
---
+++ erazer war/Assets/scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameObject mousePointA;
+    private GameObject mousePointB;
+    private GameObject arrow;
+    private GameObject circle;
+    private float currentDistance;
+    private float safeSpace;
+    private float shootPower;
+    private Vector3 shootDirection;
+    private float maxDistance =3f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mousePointA = GameObject.FindGameObjectWithTag("PointA");
+        mousePointB = GameObject.FindGameObjectWithTag("PointB");
+    }
+    private void OnMouseDrag()
+    {
+        Debug.Log(mousePointA.transform.position.x);
+        currentDistance = Vector3.Distance(mousePointA.transform.position, transform.position);
+        if (currentDistance < maxDistance)
+        {
+            safeSpace = currentDistance;
+        }else{
+            safeSpace = maxDistance;
+        }
+        shootPower = Mathf.Abs(safeSpace)*10;
+        Vector3 dimxy = mousePointA.transform.position - transform.position;
+        float difference = dimxy.magnitude;
+        mousePointB.transform.position = transform.position+((dimxy/difference)*currentDistance*-1);
+        mousePointB.transform.position = new Vector3(mousePointB.transform.position.x,mousePointB.transform.position.y,-0.8f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
