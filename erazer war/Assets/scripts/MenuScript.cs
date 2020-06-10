096315e7a4abaa994a7117e8004a000e754d715f
Salman Arshad <k163898@nu.du.pk>
2020-06-10 18:09:53 +0500

    Game UI complete

---
+++ erazer war/Assets/scripts/MenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuScript : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+    void Start()
+    {
+        // PauseMenuUI = GameObject.FindGameObjectWithTag("bgpanel");
+    }
+    public void pauseGame()
+    {
+        Debug.Log("Game Paused");
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+
+
+    }
+    // Update is called once per frame
+    public void resume()
+    {
+        Debug.Log("GG");
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    public void quit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+    public void restart(){
+        Debug.Log("reloading scene");
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
